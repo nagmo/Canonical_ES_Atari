2ee9d2bcb591b6192ebe365f912fda2a25e477af
yshildan <yshildan@gmail.com>
2019-04-16 06:25:06 +0000

    flaten queue

--- src/NS.py
+++ src/NS.py
@@ -17,7 +17,7 @@ class NS:
         self.queue.append(ob)
 
     def get_novelty_score(self):
-        pca_rep = self.pca.fit_transform(self.queue)
+        pca_rep = self.pca.fit_transform(np.asarray(self.queue))
         nbrs = self.neighbors.fit(pca_rep)
         distances, indices = nbrs.kneighbors(pca_rep[0])
         return sum(distances)
