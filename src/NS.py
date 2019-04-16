0b8b015cb3db46c032b152b46c88ee4ee5c4cd86
yshildan <yshildan@gmail.com>
2019-04-16 06:45:27 +0000

    flaten queue

--- src/NS.py
+++ src/NS.py
@@ -14,10 +14,10 @@ class NS:
 
     def add_observation(self, ob: np.ndarray):
         # ob_comp = zlib.compress(ob.tostring())
-        self.queue.append(ob)
+        self.queue.append(np.reshape(ob, (2, -1)))
 
     def get_novelty_score(self):
-        pca_rep = self.pca.fit_transform(np.reshape(self.queue, (2, -1)))
+        pca_rep = self.pca.fit_transform(self.queue)
         nbrs = self.neighbors.fit(pca_rep)
         distances, indices = nbrs.kneighbors(pca_rep[0])
         return sum(distances)
