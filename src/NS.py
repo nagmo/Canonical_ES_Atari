acc1713735c16cbd17ad7db1c08237f28ba134c8
yshildan <yshildan@gmail.com>
2019-04-17 14:18:28 +0000

    debug not printing stats

--- src/NS.py
+++ src/NS.py
@@ -22,4 +22,4 @@ class NS:
         pca_rep = self.pca.fit_transform(np.asarray(self.queue))
         nbrs = self.neighbors.fit(pca_rep)
         distances, indices = nbrs.kneighbors(np.reshape(pca_rep[0], (1, -1)))
-        return sum(distances)
+        return int(sum(distances))
