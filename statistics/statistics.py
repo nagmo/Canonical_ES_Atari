f93ad439fba7ac8d01bae49718ac58487d547d6c
yshildan <yshildan@gmail.com>
2019-04-27 07:52:25 +0000

    add final stats

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -9,10 +9,11 @@ stat_row_indexes = {
     'train_max_reward': 5
 }
 
+labels = ['step since start', 'time since start', 'eval mean reward', 'eval max reward', 'train mean reward', 'train max reward']
+
 
 def create_plots_from_file(path, output_dir):
     results = [[] for _ in range(6)]
-    labels = ['step since start', 'time since start', 'eval mean reward', 'eval max reward', 'train mean reward', 'train max reward']
     with open(path, 'r') as f:
         for line in f:
             for stat_value, res_arr in zip(line.split(','), results):
@@ -33,11 +34,33 @@ def create_plot(x, x_label, y, y_label, file_name):
     plt.savefig(file_name)
     plt.clf()
 
+
+def create_plot_from_two_files(p1, p2, output_dir):
+    r1 = [[] for _ in range(6)]
+    r2 = [[] for _ in range(6)]
+    with open(p1, 'r') as f1, open(p2, 'r') as f2:
+        for l1, l2 in zip(f1, f2):
+            for v1, a1, v2, a2 in zip(l1.split(','), r1, l2.split(','), r2):
+                a1.append(float(v1.strip()))
+                a2.append(float(v2.strip()))
+    create_two_legends_plot(r1[stat_row_indexes.get('mean_reward')], r1[stat_row_indexes.get('time')],
+                            r2[stat_row_indexes.get('mean_reward')], r2[stat_row_indexes.get('time')])
+
+
+def create_two_legends_plot(x1, y1, x2, y2):
+    fig, ax = plt.subplots()
+    ax.plot(x1, y1, 'k--', label='Model length')
+    ax.plot(x2, y2, 'k:', label='Data length')
+
+    plt.show()
+
+
 def make_stat(in_path, out_dir):
     create_plots_from_file(in_path, out_dir)
 
 
-#if __name__ == '__main__':
-#    origin_stat_path = '../logs_mpi/Qbert/Baseline/Nature/40/40/0.010000/1.000000/1.000000/None/stat.txt'
-#    create_plots_from_file(origin_stat_path, 'origin')
-
+if __name__ == '__main__':
+    origin_stat_path = '../logs_mpi/Qbert/Baseline/Nature/40/40/0.010000/1.000000/1.000000/origin/stat.txt'
+    novelty_stat_path = '../logs_mpi/Qbert/Baseline/Nature/40/40/0.010000/1.000000/1.000000/novelty/stat.txt'
+    # create_plots_from_file(origin_stat_path, 'origin')
+    create_plot_from_two_files(origin_stat_path, novelty_stat_path, '')
